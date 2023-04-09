a0ad95c933caa01b643fd1084649946c6caa7bdf
Scott Leibrand <scottleibrand@gmail.com>
2023-04-09 03:31:03 -0700

    watchOS support (runs in simulator; untested w/ voice)

---
+++ GPT Voice Chat WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPT Voice Chat WatchKit Extension
+//
+//  Created by Scott on 4/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GPT Voice Chat WatchKit Extension/GPT_Voice_ChatApp.swift
@@ -0,0 +1,19 @@
+//
+//  GPT_Voice_ChatApp.swift
+//  GPT Voice Chat WatchKit Extension
+//
+//  Created by Scott on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GPT_Voice_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
