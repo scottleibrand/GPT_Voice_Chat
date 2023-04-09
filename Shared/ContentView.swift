07ab8ea24cf1d9ee250121a49e42f8c472dcd12a
Scott Leibrand <scottleibrand@gmail.com>
2023-04-08 22:19:16 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Scott on 4/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/GPT_Voice_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT_Voice_ChatApp.swift
+//  Shared
+//
+//  Created by Scott on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GPT_Voice_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
